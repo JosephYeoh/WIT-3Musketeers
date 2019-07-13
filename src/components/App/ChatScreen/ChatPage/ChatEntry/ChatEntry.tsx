6d414f5e9235ebef977e28b70951ca7260e0391c
isabellelouwq <i.lou@student.unsw.edu.au>
2019-07-13 16:35:17 +1000

    merging chat

---
+++ src/components/App/ChatScreen/ChatPage/ChatEntry/ChatEntry.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import styles from './ChatEntry.styles';
+import ChatPage, { ConvoData } from '../ChatPage';
+import { whiteOffset } from 'src/constants/Colors';
+
+export interface Props {
+	isRight: boolean;
+	data: ConvoData;
+}
+
+export interface State {}
+
+class ChatEntry extends React.Component<Props, State> {
+	state = {};
+
+	render() {
+		const { state, props } = this;
+		return (
+			<View style={props.isRight ? styles.isRightBox : styles.isLeftBox}>
+				<Text style={props.isRight ? styles.isRightText : styles.isLeftText}>
+					{props.data.text}
+				</Text>
+			</View>
+		);
+	}
+}
+
+export default ChatEntry;
--- src/components/App/ChatScreen/ChatPage/ChatPage.tsx
+++ src/components/App/ChatScreen/ChatPage/ChatPage.tsx
@@ -1,39 +1,108 @@
+import { Ionicons } from '@expo/vector-icons';
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
-import styles from './ChatPage.styles';
-
-interface Props {
+import { KeyboardAvoidingView, ScrollView, TouchableOpacity, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { whiteOffset } from 'src/constants/Colors';
+import ChatEntry from './ChatEntry';
+export interface Props {
 	isHelpee: boolean;
 }
 
-interface ConvoData {
+export interface ConvoData {
 	name: string;
 	text: string;
-	isMe: boolean;
+	isHelpee: boolean;
 }
 
 interface State {
 	convoData: ConvoData[];
+	input: string;
 }
 
 class ChatPage extends React.Component<Props, State> {
 	state: State = {
 		convoData: [
-			{ name: 'Joseph', text: 'Hello', isMe: true },
-			{ name: 'Sabrina', text: 'Hi', isMe: false },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Alice', text: 'Hello', isHelpee: true },
+			{ name: 'Bob', text: 'Hi', isHelpee: false },
 		],
+		input: '',
 	};
+	scrollRef = React.createRef<ScrollView>();
 
 	render() {
 		const { state, props } = this;
+
+		const handlePress = () => {
+			const data: ConvoData = {
+				isHelpee: props.isHelpee,
+				name: props.isHelpee ? 'Alice' : 'Bob',
+				text: state.input,
+			};
+			const newConvoData = [...state.convoData, data];
+			this.setState({ convoData: newConvoData, input: '' });
+		};
+
 		return (
-			<View style={styles.container}>
-				{state.convoData.map((entry, key) => (
-					<Text key={key}>
-						{entry.name} says {entry.text}
-					</Text>
-				))}
-			</View>
+			<KeyboardAvoidingView style={{ flex: 1 }}>
+				<ScrollView
+					showsVerticalScrollIndicator={false}
+					ref={this.scrollRef}
+					keyboardDismissMode={'on-drag'}
+					style={{ flex: 1, margin: 8 }}
+				>
+					{state.convoData.map((entry, key) => (
+						<ChatEntry
+							data={entry}
+							key={key}
+							isRight={props.isHelpee === entry.isHelpee}
+						/>
+					))}
+				</ScrollView>
+				<View
+					style={{
+						backgroundColor: whiteOffset,
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<TextInput
+						style={{
+							flex: 1,
+							paddingVertical: 6,
+							paddingHorizontal: 8,
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+						placeholder="Type message here"
+						value={state.input}
+						onContentSizeChange={() => this.scrollRef.current!.scrollToEnd()}
+						onChangeText={value => this.setState({ input: value })}
+					/>
+					<TouchableOpacity
+						style={{ alignItems: 'center', justifyContent: 'center', margin: 4 }}
+						onPress={handlePress}
+					>
+						<View style={{ marginTop: 6, marginRight: 6 }}>
+							<Ionicons name="ios-send" size={44} color="#4285F4" />
+						</View>
+					</TouchableOpacity>
+				</View>
+			</KeyboardAvoidingView>
 		);
 	}
 }
